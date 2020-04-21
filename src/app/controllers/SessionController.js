9577de7696031209342f6f0e7ed0047b212a4835
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-04-21 14:12:00 -0300

    JWT installed and session controller is generating a token

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,31 +1,41 @@
-const UserModel = require('../models/UserModel.js');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+
+const UserModel = require('../models/UserModel.js');
+const authConfiguration = require('../../config/authentication.js');
 
 class SessionController{
   async store(req,res){
-    const { email, password } = req.body;
+    const { email : email_body, password } = req.body;
 
     /**
      * Verifyng if user exists
      */
 
-    const results = await UserModel.findByEmail(email);
+    const results = await UserModel.findByEmail(email_body);
     if(results.rows.length != 1){
       return res.status(400).json({ error: 'Email does not exists'});
     }
+
+    const { id, email, password_hash } = results.rows[0];
 
     /**
      * Verifying if pass match
      */
 
-    const compared = await bcrypt.compare(password, results.rows[0].password_hash);
+    const compared = await bcrypt.compare(password, password_hash);
     if(!compared){
       return res.status(400).json({ error : 'Password does not match'});
     }
 
     return res.json({
-      email,
-      password
+      user: {
+        email,
+        password
+      },
+      token: jwt.sign({ id },authConfiguration.secret, {
+        expiresIn: authConfiguration.expiresIn,
+      })
     });
 
   }
