6fd8e8f955ff07d32d88db4798027f413dbb2d21
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-15 17:55:23 -0300

    card styled

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
+import { Route, BrowserRouter, Switch} from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
 
 function App() {
   return (
+    <>
     <Header />
+    <BrowserRouter>
+      <Switch>
+        <Route isPrivate path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ web/src/pages/Home/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Cidade from '../../assets/cidade.jpg';
+
+import './styles.css';
+
+function Home() {
+
+  return (
+    <div className="container">
+      <h1>Cidades</h1>
+      <div className="cards">
+        <div className="card">
+          <img src={Cidade} alt="cidade"/>
+          <div className="text">
+            <p className="little">4 Days ago</p>
+            <h3>Post One</h3>
+            <p className="text-info">Lorsdfsdfsdfsdfsdfsdsfdem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="info">
+            <ul>
+              <li>
+                <p>4m</p>
+                <span>READ</span>
+              </li>
+              <li>
+                <p>52323</p>
+                <span>VIEWS</span>
+              </li>
+              <li>
+                <p>32</p>
+                <span>COMMENTS</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={Cidade} alt="cidade"/>
+          <div className="text">
+            <p className="little">4 Days ago</p>
+            <h3>Post One</h3>
+            <p className="text-info">Lorsdfsdfsdfsdfsdfsdsfdem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="info">
+            <ul>
+              <li>
+                <p>4m</p>
+                <span>READ</span>
+              </li>
+              <li>
+                <p>52323</p>
+                <span>VIEWS</span>
+              </li>
+              <li>
+                <p>32</p>
+                <span>COMMENTS</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={Cidade} alt="cidade"/>
+          <div className="text">
+            <p className="little">4 Days ago</p>
+            <h3>Post One</h3>
+            <p className="text-info">Lorsdfsdfsdfsdfsdfsdsfdem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+          <div className="info">
+            <ul>
+              <li>
+                <p>4m</p>
+                <span>READ</span>
+              </li>
+              <li>
+                <p>52323</p>
+                <span>VIEWS</span>
+              </li>
+              <li>
+                <p>32</p>
+                <span>COMMENTS</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
