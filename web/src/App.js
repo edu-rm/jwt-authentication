b9a807efbe485866ec244c36b8c5bd096d67beb6
Eduardo Rampon <eduardomeireles3232@gmail.com>
2020-06-15 14:55:49 -0300

    Header

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>HelloWorld</h1>
+    <Header />
   );
 }
 
---
+++ web/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <h1>HOOKS</h1>
+          <p>By: Eduardo Rampon Meireles</p>
+        </li>
+        <li>
+          <p>Login/Sair</p>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
